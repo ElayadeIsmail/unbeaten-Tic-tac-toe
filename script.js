8a697f9f5e770c71a83b8b6f48993b987e570ac0
sam-Esmail <sam@ISMAILs-MacBook-Pro.local>
2020-04-29 22:48:31 +0000

    Tic toc with  damn ass computer

--- script.js
+++ script.js
@@ -9,21 +9,35 @@ const sq7 = document.getElementById("squer-7");
 const sq8 = document.getElementById("squer-8");
 const sq9 = document.getElementById("squer-9");
 const end = document.getElementById("end");
-
-let player = 2;
+let player = 1;
+let items = [];
 
 function playGame(clickedEl) {
   if (clickedEl.className === "squer") {
-    if (!(player % 2)) {
-      clickedEl.innerText = "X";
-    } else {
-      clickedEl.innerText = "O";
+    clickedEl.innerText = "X";
+    if (!checkIfWin()) {
+      setTimeout(RandomClick, 200);
     }
   }
-  checkIfWin();
+  checkIfWin("Win");
 }
 
-function checkIfWin() {
+function RandomClick() {
+  const data = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9];
+
+  data.forEach((item) => {
+    if (item.innerText === "") {
+      items.push(item);
+    }
+  });
+
+  if (items.length > 0) {
+    items[Math.floor(Math.random() * items.length)].innerText = "O";
+  }
+  checkIfWin("Lost");
+}
+
+function checkIfWin(msg) {
   const term1 = sq1.innerText;
   const term2 = sq2.innerText;
   const term3 = sq3.innerText;
@@ -43,9 +57,13 @@ function checkIfWin() {
     (term1 === term5 && term1 === term9 && (term1 === "X" || term1 === "O")) ||
     (term7 === term5 && term7 === term3 && (term7 === "X" || term7 === "O"))
   ) {
-    document.body.innerHTML = `<span>Congrats You Win ..</span>
-    
-     <button class='play-again' id="play-again"/>Play Again</button>`;
+    end.innerHTML = ` <span> You ${msg}</span>
+    <button class='play-again' id="play-again"/>Play Again</button>
+     `;
+    pitch.classList.add("end-game");
+    // document.body.innerHTML = `<span> You're ${msg} ..</span>
+    //  <button class='play-again' id="play-again"/>Play Again</button>`;
+    return true;
   } else if (
     term1 !== "" &&
     term2 !== "" &&
@@ -60,6 +78,8 @@ function checkIfWin() {
     document.body.innerHTML = `<span>Try Again</span>
     
      <button class='play-again' id="play-again"/>Play Again</button>`;
+  } else {
+    return false;
   }
 }
 
@@ -67,9 +87,11 @@ pitch.addEventListener("click", (e) => {
   const clickedEl = e.target;
   if (clickedEl.innerText === "") {
     playGame(clickedEl);
-    player = player + 1;
+    // player = player + 1;
+    items = [];
   }
 });
+
 document.body.addEventListener("click", (e) => {
   if (e.target.classList.contains("play-again")) {
     window.location.reload();
