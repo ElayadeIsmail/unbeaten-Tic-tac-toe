3df68ef5b62c4abcf6df49002ece30bb627c84fe
sam-Esmail <sam@ISMAILs-MacBook-Pro.local>
2020-05-15 17:08:16 +0000

    You will Never Win

--- script.js
+++ script.js
@@ -1,99 +1,135 @@
-const pitch = document.getElementById("tic-toc");
-const sq1 = document.getElementById("squer-1");
-const sq2 = document.getElementById("squer-2");
-const sq3 = document.getElementById("squer-3");
-const sq4 = document.getElementById("squer-4");
-const sq5 = document.getElementById("squer-5");
-const sq6 = document.getElementById("squer-6");
-const sq7 = document.getElementById("squer-7");
-const sq8 = document.getElementById("squer-8");
-const sq9 = document.getElementById("squer-9");
+const squers = document.querySelectorAll(".squer");
 const end = document.getElementById("end");
-let player = 1;
-let items = [];
+let origBoard;
+const huPlayer = "O";
+const aiPlayer = "X";
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+];
 
-function playGame(clickedEl) {
-  if (clickedEl.className === "squer") {
-    clickedEl.innerText = "X";
-    if (!checkIfWin()) {
-      setTimeout(RandomClick, 200);
-    }
-  }
-  checkIfWin("Win");
+startGame();
+
+function startGame() {
+  origBoard = Array.from(Array(9).keys());
+  end.style.display = "none";
+  console.log(origBoard);
+  squers.forEach((squer) => {
+    squer.innerText = "";
+    squer.addEventListener("click", handleClick);
+    squer.style.removeProperty("background-color");
+  });
 }
 
-function RandomClick() {
-  const data = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9];
+function handleClick(squer) {
+  if (typeof origBoard[squer.target.id] == "number") {
+    playTurn(squer.target.id, huPlayer);
+    if (!tie()) playTurn(bestSpot(), aiPlayer);
+  }
+}
 
-  data.forEach((item) => {
-    if (item.innerText === "") {
-      items.push(item);
+function tie() {
+  if (emptySquares().length == 0) {
+    squers.forEach((squer) => {
+      squer.style.setProperty("background-color", "blue");
+      squer.removeEventListener("click", handleClick);
+    });
+    end.style.display = "flex";
+
+    end.innerHTML = ` <h2>Try Again It's a Tie<h2>
+      <button class="play-again" onClick="startGame()">Play Again</button>`;
+    return true;
+  }
+  return false;
+}
+
+function playTurn(id, player) {
+  origBoard[id] = player;
+  document.getElementById(id).innerText = player;
+  let gameWin = checkWin(origBoard, player);
+  if (gameWin) gameOver(gameWin);
+}
+
+function checkWin(board, player) {
+  const plays = board.reduce((a, b, i) => (b == player ? a.concat(i) : a), []);
+  let gameWin = null;
+  winCombos.forEach((win, index) => {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWin = { player, index };
+      return;
     }
   });
-
-  if (items.length > 0) {
-    items[Math.floor(Math.random() * items.length)].innerText = "O";
-  }
-  checkIfWin("Lost");
+  return gameWin;
 }
 
-function checkIfWin(msg) {
-  const term1 = sq1.innerText;
-  const term2 = sq2.innerText;
-  const term3 = sq3.innerText;
-  const term4 = sq4.innerText;
-  const term5 = sq5.innerText;
-  const term6 = sq6.innerText;
-  const term7 = sq7.innerText;
-  const term8 = sq8.innerText;
-  const term9 = sq9.innerText;
-  if (
-    (term1 === term2 && term1 === term3 && (term1 === "X" || term1 === "O")) ||
-    (term1 === term4 && term1 === term7 && (term1 === "X" || term1 === "O")) ||
-    (term4 === term5 && term4 === term6 && (term4 === "X" || term4 === "O")) ||
-    (term7 === term8 && term7 === term9 && (term7 === "X" || term7 === "O")) ||
-    (term2 === term5 && term2 === term8 && (term2 === "X" || term2 === "O")) ||
-    (term3 === term6 && term3 === term9 && (term3 === "X" || term3 === "O")) ||
-    (term1 === term5 && term1 === term9 && (term1 === "X" || term1 === "O")) ||
-    (term7 === term5 && term7 === term3 && (term7 === "X" || term7 === "O"))
-  ) {
-    end.innerHTML = ` <span> You ${msg}</span>
-    <button class='play-again' id="play-again"/>Play Again</button>
-     `;
-    pitch.classList.add("end-game");
-    // document.body.innerHTML = `<span> You're ${msg} ..</span>
-    //  <button class='play-again' id="play-again"/>Play Again</button>`;
-    return true;
-  } else if (
-    term1 !== "" &&
-    term2 !== "" &&
-    term3 !== "" &&
-    term4 !== "" &&
-    term5 !== "" &&
-    term6 !== "" &&
-    term7 !== "" &&
-    term8 !== "" &&
-    term9 !== ""
-  ) {
-    document.body.innerHTML = `<span>Try Again</span>
-    
-     <button class='play-again' id="play-again"/>Play Again</button>`;
+function gameOver(gameWin) {
+  winCombos[gameWin.index].forEach((id) => {
+    document.getElementById(id).style.backgroundColor =
+      gameWin.player == huPlayer ? "green" : "red";
+  });
+  squers.forEach((squer) => squer.removeEventListener("click", handleClick));
+  msg = gameWin.player == "O" ? "Congrats You Win" : "Unfortunately You Lost";
+  end.style.display = "flex";
+
+  end.innerHTML = ` <h2>${msg}<h2>
+    <button class="play-again" onClick="startGame()">Play Again</button>`;
+}
+
+function emptySquares() {
+  return origBoard.filter((board) => typeof board == "number");
+}
+
+function bestSpot() {
+  return minimax(origBoard, aiPlayer).index;
+}
+
+function minimax(newBoard, player) {
+  let availSpots = emptySquares(newBoard);
+  if (checkWin(newBoard, player)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+  let moves = [];
+  for (let i = 0; i < availSpots.length; i++) {
+    let move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+    if (player == aiPlayer) {
+      let result = minimax(newBoard, huPlayer);
+      move.score = result.score;
+    } else {
+      let result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+    newBoard[availSpots[i]] = move.index;
+    moves.push(move);
+  }
+  let bestMove;
+  if (player == aiPlayer) {
+    let bestScore = -10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
   } else {
-    return false;
+    let bestScore = 10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
   }
+  return moves[bestMove];
 }
-
-pitch.addEventListener("click", (e) => {
-  const clickedEl = e.target;
-  if (clickedEl.innerText === "") {
-    playGame(clickedEl);
-    // player = player + 1;
-    items = [];
-  }
-});
-
-document.body.addEventListener("click", (e) => {
-  if (e.target.classList.contains("play-again")) {
-    window.location.reload();
-  }
-});
